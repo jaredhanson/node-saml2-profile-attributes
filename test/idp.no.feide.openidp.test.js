7680f41c22f7f2f8e5dcc67931c540e93bfc0977
Jared Hanson <jaredhanson@gmail.com>
2013-07-27 19:24:02 -0700

    Parse common name from assertion using X.500 attributes.

---
+++ test/idp.no.feide.openidp.test.js
@@ -0,0 +1,16 @@
+var profile = require('index')
+  , fs = require('fs');
+
+describe('attribute statment issued by https://openidp.feide.no', function() {
+    
+  var xml = fs.readFileSync(__dirname + '/data/openidp.feide.no/1.xml', 'utf8');
+    
+  it('should parse correctly', function() {
+    var prof = profile.parse(xml);
+    
+    expect(prof).to.be.an('object');
+    expect(prof.name).to.be.an('object');
+    expect(prof.name.formatted).to.equal('Jared Hanson');
+  });
+  
+});
