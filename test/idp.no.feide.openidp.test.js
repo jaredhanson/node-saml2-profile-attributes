0d72bc795e7bb5f6fe9753663cbceed536412775
Jared Hanson <jaredhanson@gmail.com>
2013-07-27 20:30:51 -0700

    Additional profile parsing.

--- test/idp.no.feide.openidp.test.js
+++ test/idp.no.feide.openidp.test.js
@@ -9,8 +9,11 @@ describe('attribute statment issued by https://openidp.feide.no', function() {
     var prof = profile.parse(xml);
     
     expect(prof).to.be.an('object');
+    expect(prof.username).to.equal('jaredhanson');
     expect(prof.name).to.be.an('object');
     expect(prof.name.formatted).to.equal('Jared Hanson');
+    expect(prof.name.familyName).to.equal('Hanson');
+    expect(prof.name.givenName).to.equal('Jared');
   });
   
 });
