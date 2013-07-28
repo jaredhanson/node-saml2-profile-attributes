4a8ae95cfd14a9af020f6da8a027386c3508af1e
Jared Hanson <jaredhanson@gmail.com>
2013-07-27 22:31:01 -0700

    Parse email address.

--- test/idp.no.feide.openidp.test.js
+++ test/idp.no.feide.openidp.test.js
@@ -14,6 +14,10 @@ describe('attribute statment issued by https://openidp.feide.no', function() {
     expect(prof.name.formatted).to.equal('Jared Hanson');
     expect(prof.name.familyName).to.equal('Hanson');
     expect(prof.name.givenName).to.equal('Jared');
+    expect(prof.emails).to.be.an('array');
+    expect(prof.emails).to.have.length(1);
+    expect(prof.emails[0]).to.be.an('object');
+    expect(prof.emails[0].value).to.equal('jaredhanson@example.com');
   });
   
 });
